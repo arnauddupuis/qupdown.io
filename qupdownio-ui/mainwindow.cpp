a615b5c13304e9b58a5360175376f192c961c587
Arnaud Dupuis <a.dupuis@infinityperl.org>
2013-06-28 08:33:49 +0200

    Update the test UI to match changes in libqupdownio.

--- qupdownio-ui/mainwindow.cpp
+++ qupdownio-ui/mainwindow.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <QDebug>
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -10,6 +11,9 @@ MainWindow::MainWindow(QWidget *parent) :
     m_qupdownio = new Qupdownio(this);
     connect(m_qupdownio,SIGNAL(checksFinished(QList<LibQupdownio::Check*>)),this,SLOT(on_checksFinished(QList<LibQupdownio::Check*>)));
     connect(m_qupdownio,SIGNAL(downtimesFinished(QList<LibQupdownio::CheckError*>)),this,SLOT(on_downtimesFinished(QList<LibQupdownio::CheckError*>)));
+    connect( m_qupdownio,SIGNAL(updateCheckFinished(LibQupdownio::Check*)),this,SLOT(on_updateFinished(LibQupdownio::Check*)) );
+    connect( m_qupdownio,SIGNAL(deleteCheckFinished(bool,QString)),this,SLOT(on_deleteFinished(bool,QString)) );
+    connect( m_qupdownio,SIGNAL(addCheckFinished(LibQupdownio::Check*)),this,SLOT(on_addCheckFinished(LibQupdownio::Check*)) );
     m_shadow = new QGraphicsDropShadowEffect(ui->menuWidget);
 }
 
@@ -38,6 +42,36 @@ void MainWindow::on_downtimesFinished(QList<LibQupdownio::CheckError *> p_list){
 	foreach( LibQupdownio::CheckError *ce, p_list ){
 		qDebug() << "Downtime: start: " << ce->startedAt().toString("yyyy-MM-dd@hh:mm:ss") << " duration: " << ce->duration() << " error: " << ce->error() << " end: " << ce->endedAt().toString("yyyy-MM-dd@hh:mm:ss");
 	}
+}
+
+void MainWindow::on_updateFinished(LibQupdownio::Check *p_check){
+	qDebug() << "Check updated with: URL=" << p_check->url() << " period=" << p_check->period() << " published=" << p_check->published() << "token=" << p_check->token();
+	QMessageBox msg;
+	msg.setText( "Check updated with: URL=" + p_check->url().toString() + " period=" + p_check->period() + " published=" + p_check->published() + "token=" + p_check->token() );
+	msg.setIcon(QMessageBox::Information);
+	msg.exec();
+}
+
+void MainWindow::on_addCheckFinished(LibQupdownio::Check *p_check){
+//	qDebug() << "Check added with: URL=" << p_check->url() << " period=" << p_check->period() << " published=" << p_check->published() << "token=" << p_check->token();
+	on_checksPushButton_clicked();
+	QMessageBox msg;
+	msg.setText( "Check added with: URL=" + p_check->url().toString() + " period=" + p_check->period() + " published=" + p_check->published() + "token=" + p_check->token() );
+	msg.setIcon(QMessageBox::Information);
+	msg.exec();
+}
+
+void MainWindow::on_deleteFinished(bool status, QString message){
+	QMessageBox msg;
+	msg.setText(message);
+	if(status){
+		msg.setIcon(QMessageBox::Information);
+		on_checksPushButton_clicked();
+	}
+	else{
+		msg.setIcon(QMessageBox::Critical);
+	}
+	msg.exec();
 }
 
 void MainWindow::on_addCheckPushButton_clicked(){
--- qupdownio-ui/mainwindow.h
+++ qupdownio-ui/mainwindow.h
@@ -11,25 +11,28 @@ class MainWindow;
 
 class MainWindow : public QMainWindow
 {
-    Q_OBJECT
-    
+	Q_OBJECT
+
 public:
-    explicit MainWindow(QWidget *parent = 0);
-    ~MainWindow();
-    
+	explicit MainWindow(QWidget *parent = 0);
+	~MainWindow();
+
 private slots:
 	void on_checksPushButton_clicked();
-    void on_downtimesPushButton_clicked();
-    void on_addCheckPushButton_clicked();
-    void on_updateCheckPushButton_clicked();
-    void on_deleteCheckPushButton_clicked();
-    void on_checksFinished(QList<LibQupdownio::Check*> p_list);
-    void on_downtimesFinished(QList<LibQupdownio::CheckError*> p_list);
+	void on_downtimesPushButton_clicked();
+	void on_addCheckPushButton_clicked();
+	void on_updateCheckPushButton_clicked();
+	void on_deleteCheckPushButton_clicked();
+	void on_checksFinished(QList<LibQupdownio::Check*> p_list);
+	void on_downtimesFinished(QList<LibQupdownio::CheckError*> p_list);
+	void on_updateFinished(LibQupdownio::Check* p_check);
+	void on_deleteFinished(bool status, QString message);
+	void on_addCheckFinished(LibQupdownio::Check*);
 
 private:
-    Ui::MainWindow *ui;
-    Qupdownio *m_qupdownio;
-    QGraphicsDropShadowEffect *m_shadow;
+	Ui::MainWindow *ui;
+	Qupdownio *m_qupdownio;
+	QGraphicsDropShadowEffect *m_shadow;
 
 };
 
